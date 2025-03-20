be986045a230f6ecbc0c543c971dae4a077e6f08
MelodyMad <M_M337@student.usc.edu.au>
2025-03-20 23:11:51 +1000

    SpeedController C# Script
    A c# script was created to control the speed and direction of any object that this script is applied to. This script references the Movement script. The SpeedController script and the Movement script were both added to Spaceship player in Unity under the inspector.

--- Assets/Scripts/SpeedController.cs
+++ Assets/Scripts/SpeedController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class SpeedController : MonoBehaviour
 {
+    // Create a variable of the direction of a 2D vector
     [SerializeField]
     private Vector2 direction;
+    // Create a variable from the Movement script named movement
+    [SerializeField]
+    private Movement movement;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,7 @@ public class SpeedController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Controlable direction that any object can use
+        movement.Move(direction);
     }
 }
